2a8959f08133d8e07a5e13a4436d59ffc3bf399b
Ivan <verba447@gmail.com>
2026-07-22 00:17:23 +0200

    Init Spring

---
+++ src/main/java/viindustries/fuel_search/FuelSearchApplication.java
@@ -0,0 +1,13 @@
+package viindustries.fuel_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/viindustries/fuel_search/FuelSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package viindustries.fuel_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
